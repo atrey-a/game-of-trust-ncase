f2d72a4e2cece4dc05482218f886e6e3c2ae3b9c
atreyasaurabh <atreyasaurabh@gmail.com>
2022-09-27 17:39:35 +0530

    Commit Two

--- back.py
+++ back.py
@@ -14,11 +14,13 @@ class Screen:
         pygame.display.set_icon(pygame.image.load(logo_path))
         pygame.display.set_caption(caption)
         self.fill_color = pygame.Color(color_name)
+        self.hovering = -1
+        self.active = -1
     def before(self):
         self.screen.fill(self.fill_color)
     def after(self):
         pygame.display.flip()
-    def event_handler(self):
+    def event_handler(self,buttons,reloader):
         while 1:
             state_change = 0
             for event in pygame.event.get():
@@ -33,17 +35,43 @@ class Screen:
                     self.screen = pygame.display.set_mode(self.size,pygame.RESIZABLE)
                     state_change = 1
                 elif event.type == pygame.MOUSEMOTION:
-                    state_change = 1
+                    if self.hovering!=-1:
+                        if ((buttons[self.hovering][0]) and (not pygame.Rect((buttons[self.hovering][1][0]+self.subscreen_position[0],buttons[self.hovering][1][1]+self.subscreen_position[1],buttons[self.hovering][1][2],buttons[self.hovering][1][3])).collidepoint(pygame.mouse.get_pos()))):
+                            buttons[self.hovering][0]=0
+                            self.hovering = -1
+                            reloader()
+                            state_change = 1
+                    else:
+                        for i in range(len(buttons)):
+                            if pygame.Rect((buttons[i][1][0]+self.subscreen_position[0],buttons[i][1][1]+self.subscreen_position[1],buttons[i][1][2],buttons[i][1][3])).collidepoint(pygame.mouse.get_pos()):
+                                buttons[i][0]=1
+                                self.hovering = i
+                                reloader()
+                                state_change = 1
+                                break
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    state_change = 1
+                    if self.hovering!=-1:
+                        if (buttons[self.hovering][0]==1):
+                            buttons[self.hovering][0]=2
+                            self.active = self.hovering
+                            reloader()
+                            state_change = 1
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    state_change = 1
+                    if self.active!=-1:
+                        if (buttons[self.active][0]==2 and self.active==self.hovering):
+                            buttons[self.active][0]=3
+                            reloader()
+                            buttons[self.active][7][0](*buttons[self.active][7][1:])
+                            state_change = 1
             if state_change==1:
                 break
+    def flush(self):
+        self.hovering = -1
+        self.active = -1
     def place_subscreen(self,subscreen):
         size_of_subscreen = subscreen.get_size()
-        position = ((self.size[0]-size_of_subscreen[0])/2,(self.size[1]-size_of_subscreen[1])/2)
-        self.screen.blit(subscreen,position)
+        self.subscreen_position = ((self.size[0]-size_of_subscreen[0])/2,(self.size[1]-size_of_subscreen[1])/2)
+        self.screen.blit(subscreen,self.subscreen_position)
 
 class Section:
     def __init__(self,size,color_name,initial_state=False):
@@ -53,22 +81,41 @@ class Section:
         self.labels = []
         self.buttons = []
         self.objects = []
+        self.active_position = [0,0]
     #Check for invalid/unavailable fonts
-    def add_labels(self,*lables): #(text,color_name,font_name,font_size,position(strictly tuple),line_break)
-        self.active_position = list(lables[0][4])
-        for label in lables:
-            font_object = pygame.font.Font(pygame.font.match_font(label[2]),label[3])
-            if label[5]:
-                self.active_position[0] = lables[0][4][0]
-                self.active_position[1] += label[3]*1.5
-                print(lables[0][4][0])
-            img = font_object.render(label[0],1,pygame.Color(label[1]))
-            copy_position = (self.active_position[0],self.active_position[1])
-            self.labels.append([img,copy_position])
-            self.active_position[0]+=img.get_width()
-    def ready_subscreen(self):
+    #For new label, offset = 0; For new line offset = line gap in terms of font size; For continuation leave undeclared
+    def add_label(self,text,text_color,font_name,font_size,position,offset=-1):
+        if (offset!=-1):
+            self.active_position= [position[0],self.active_position[1]+offset*font_size]
+        font_object = pygame.font.Font(pygame.font.match_font(font_name),font_size)
+        img = font_object.render(text,1,pygame.Color(text_color))
+        copy_position = (self.active_position[0],self.active_position[1])
+        self.labels.append([img,copy_position])
+        self.active_position[0]+=img.get_width()
+    def add_button(self,text,text_color,font_name,font_size,rect,passive_bg,active_bg,passive_border,active_border,funcset):
+        font_object = pygame.font.Font(pygame.font.match_font(font_name),font_size)
+        text_img = font_object.render(text,1,pygame.Color(text_color))
+        self.buttons.append([0,rect,text_img,pygame.Color(passive_bg),pygame.Color(active_bg),pygame.Color(passive_border),pygame.Color(active_border),funcset])
+        #First integer: 0=none, 1=hover, 2=active, 3=done
+    def ready_static(self):
         for label in self.labels:
-            print(label[1])
             self.background.blit(label[0],label[1])
+    def ready_dynamic(self):
+        for button in self.buttons:
+            if button[0]==0:
+                pygame.draw.rect(self.background,button[3],button[1],border_radius=int(button[1][1]/2))
+                pygame.draw.rect(self.background,button[5],button[1],width=2,border_radius=int(button[1][1]/2))
+            elif button[0]==1:
+                pygame.draw.rect(self.background,button[3],button[1],border_radius=int(button[1][1]/2))
+                pygame.draw.rect(self.background,button[6],button[1],width=2,border_radius=int(button[1][1]/2))
+            elif button[0]==2:
+                pygame.draw.rect(self.background,button[4],button[1],border_radius=int(button[1][1]/2))
+                pygame.draw.rect(self.background,button[6],button[1],width=2,border_radius=int(button[1][1]/2))
+            elif button[0]==3:
+                pygame.draw.rect(self.background,button[4],button[1],border_radius=int(button[1][1]/2))
+                pygame.draw.rect(self.background,button[5],button[1],width=2,border_radius=int(button[1][1]/2))            
+            self.background.blit(button[2],(button[1][0]+((button[1][2]-button[2].get_width())/2),button[1][1]+((button[1][3]-button[2].get_height())/2)))
+    def flush(self):
+        pass
     def return_subscreen(self):
         return self.background
--- main.py
+++ main.py
@@ -1,18 +1,67 @@
 def main():
-    import back
+    import back    
     
     s = back.Screen((1200,700),(850,650),"logo.png","Window Caption")
-    
-    L1 = back.Section((800,600),'white',True)
-    L1.add_labels(["Lorem ipsum","blue","bahnschrift",24,(0,0),0],[" dolor","red","bahnschrift",24,[0,0],0])
-    L1.ready_subscreen()
 
-    while 1:
+    def switch_section(f,t):
+        f.enabled = False
+        t.enabled = True
+        t.ready_static()
+        s.flush()
+    
+    L1 = back.Section((800,600),'white',False)
+    L2 = back.Section((800,600),'white',False)
+    L3 = back.Section((800,600),'white',False)
+    L4 = back.Section((800,600),'white',False)
+
+    L1.add_label("First ","black","arial",36,(0,0),0)
+    L1.add_label("Page","red","arial",36,(0,0))
+    L1.add_label("Content","blue","arial",36,(0,0),1.2)
+    L1.add_button("Go to L2","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L1,L2])
+    L1.ready_static()
+    L1.ready_dynamic()
+    
+    L2.add_label("Second ","black","arial",48,(0,0),0)
+    L2.add_label("Page","orange","arial",48,(0,0))
+    L2.add_label("Content","blue","arial",48,(0,0),1.2)
+    L2.add_button("Go to L3","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L2,L3])
+    L2.ready_static()
+    L2.ready_dynamic()
+    
+    L3.add_label("Third ","red","palatinolinotype",48,(0,0),0)
+    L3.add_label("Page","green","palatinolinotype",48,(0,0))
+    L3.add_label("Content","black","palatinolinotype",48,(0,0),1.2)
+    L3.add_button("Go to L4","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L3,L4])
+    L3.ready_static()
+    L3.ready_dynamic()
+    
+    L4.add_button("Go to L1","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L4,L1])
+    L4.ready_static()
+    L4.ready_dynamic()
+
+    switch_section(L1,L1)
+
+    while 1:        
         while L1.enabled:
             s.before()
             s.place_subscreen(L1.return_subscreen())
             s.after()
-            s.event_handler()
+            s.event_handler(L1.buttons,L1.ready_dynamic)
+        while L2.enabled:
+            s.before()
+            s.place_subscreen(L2.return_subscreen())
+            s.after()
+            s.event_handler(L2.buttons,L2.ready_dynamic)
+        while L3.enabled:
+            s.before()
+            s.place_subscreen(L3.return_subscreen())
+            s.after()
+            s.event_handler(L3.buttons,L3.ready_dynamic)
+        while L4.enabled:
+            s.before()
+            s.place_subscreen(L4.return_subscreen())
+            s.after()
+            s.event_handler(L4.buttons,L4.ready_dynamic)
 
 if __name__ == "__main__":
     main()
