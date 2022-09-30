3f193067569ec7f600221a1b8933126b2f63d626
atreyasaurabh <atreyasaurabh@gmail.com>
2022-09-30 06:55:56 +0530

    Commit Three

--- back.py
+++ back.py
@@ -1,26 +1,31 @@
 import pygame
 
+factor = 1
 pygame.init()
+pygame.freetype.set_default_resolution(72*factor)
 
 def end():
     pygame.quit()
     exit(1)
 
 class Screen:
-    def __init__(self,initial_size,minimum_size,logo_path,caption,color_name='black'):
+    def __init__(self,initial_size,minimum_size,logo_path,caption,color_name='black',navbar=[]):
         self.screen = pygame.display.set_mode(initial_size,pygame.RESIZABLE)
         self.size = list(initial_size)
         self.minimum_size = minimum_size
         pygame.display.set_icon(pygame.image.load(logo_path))
         pygame.display.set_caption(caption)
-        self.fill_color = pygame.Color(color_name)
+        self.fill_color = color_name
         self.hovering = -1
         self.active = -1
     def before(self):
-        self.screen.fill(self.fill_color)
+        self.screen.fill(self.fill_color)        
     def after(self):
         pygame.display.flip()
-    def event_handler(self,buttons,reloader):
+    def event_handler(self,buttons,reloaders,refresher=0):
+        def reloader():
+            for f in reloaders:
+                f()
         while 1:
             state_change = 0
             for event in pygame.event.get():
@@ -59,11 +64,19 @@ class Screen:
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if self.active!=-1:
                         if (buttons[self.active][0]==2 and self.active==self.hovering):
-                            buttons[self.active][0]=3
+                            buttons[self.active][0]=0
                             reloader()
-                            buttons[self.active][7][0](*buttons[self.active][7][1:])
+                            for f in buttons[self.active][8]:
+                                f[0](*f[1:])
                             state_change = 1
-            if state_change==1:
+            if refresher:
+                for i in range(len(buttons)):
+                    if pygame.Rect((buttons[i][1][0]+self.subscreen_position[0],buttons[i][1][1]+self.subscreen_position[1],buttons[i][1][2],buttons[i][1][3])).collidepoint(pygame.mouse.get_pos()):
+                        buttons[i][0]=1
+                        self.hovering = i
+                        reloader()
+                        state_change = 1
+            if state_change:
                 break
     def flush(self):
         self.hovering = -1
@@ -74,48 +87,103 @@ class Screen:
         self.screen.blit(subscreen,self.subscreen_position)
 
 class Section:
-    def __init__(self,size,color_name,initial_state=False):
+    def __init__(self,size,color_name,caption,initial_state=False):
         self.background = pygame.Surface(size)
-        self.background.fill(pygame.Color(color_name))
+        self.background.fill(color_name)
+        self.background_color = color_name
+        self.caption = caption
         self.enabled = initial_state
         self.labels = []
         self.buttons = []
         self.objects = []
         self.active_position = [0,0]
-    #Check for invalid/unavailable fonts
-    #For new label, offset = 0; For new line offset = line gap in terms of font size; For continuation leave undeclared
-    def add_label(self,text,text_color,font_name,font_size,position,offset=-1):
-        if (offset!=-1):
-            self.active_position= [position[0],self.active_position[1]+offset*font_size]
-        font_object = pygame.font.Font(pygame.font.match_font(font_name),font_size)
-        img = font_object.render(text,1,pygame.Color(text_color))
+    #For new label, offset = 0; For new line offset = line gap in terms of font size; For continuation offset = -1; For horizontally centering single label, offset = -2
+    def add_label(self,text,text_color,font_name,font_size,position,offset=-1,italic=False,bold=False,background="white"):
+        font_object = pygame.freetype.SysFont(font_name,int(font_size/factor),bold=bold,italic=italic) 
+        font_object.antialiased = True
+        img = font_object.render(text,text_color,bgcolor=background)[0]
+        if (offset==0):
+            self.active_position= [position[0],position[1]]
+        elif (offset==-2):
+            self.active_position = [position[0]-(img.get_width()/2),position[1]]
+        elif (offset!=-1):
+            self.active_position= [position[0],self.active_position[1]+offset*font_size]        
         copy_position = (self.active_position[0],self.active_position[1])
-        self.labels.append([img,copy_position])
+        self.labels.append([img,copy_position,text])
         self.active_position[0]+=img.get_width()
-    def add_button(self,text,text_color,font_name,font_size,rect,passive_bg,active_bg,passive_border,active_border,funcset):
-        font_object = pygame.font.Font(pygame.font.match_font(font_name),font_size)
-        text_img = font_object.render(text,1,pygame.Color(text_color))
-        self.buttons.append([0,rect,text_img,pygame.Color(passive_bg),pygame.Color(active_bg),pygame.Color(passive_border),pygame.Color(active_border),funcset])
-        #First integer: 0=none, 1=hover, 2=active, 3=done
+    def remove_label(self,text):
+        for label in self.labels:
+            if label[2]==text:
+                self.labels.remove(label)
+    def add_button(self,text,text_color,font_name,font_size,rect,passive_bg,hover_bg,active_bg,border_color,funcset,border_width=1):
+        font_object = pygame.freetype.SysFont(font_name,int(font_size/factor))
+        font_object.antialiased = True
+        self.buttons.append([0,rect,[text,text_color,font_object],passive_bg,hover_bg,active_bg,border_color,border_width,funcset])
+        #First integer: 0=none, 1=hover, 2=active
+    def remove_button(self,text):
+        for button in self.buttons:
+            if button[2][0]==text:
+                self.buttons.remove(button)
+                break
+    def config_machine(self,rect,left,right,top,bottom,leftn=None,rightn=None,topn=None,bottomn=None,color="grey"):
+        for obj in self.objects:
+            if obj[2]=="machine":
+                self.objects.remove(obj)
+        mach = pygame.Surface((rect[2],rect[3]))
+        mach.fill(color)
+        backcolors = ["white","yellow"]
+        #pygame.draw.polygon(mach,backcolors[top],[(125,20),(205,100),(125,180),(45,100)])
+        pygame.draw.polygon(mach,backcolors[top],[(125,20),(165,60),(125,100),(85,60)])        
+        pygame.draw.polygon(mach,backcolors[right],[(165,60),(205,100),(165,140),(125,100)])
+        pygame.draw.polygon(mach,backcolors[bottom],[(125,100),(165,140),(125,180),(85,140)])
+        pygame.draw.polygon(mach,backcolors[left],[(85,60),(125,100),(85,140),(45,100)])        
+        pygame.draw.polygon(mach,"black",[(125,20),(205,100),(125,180),(45,100)],width=5)
+        pygame.draw.lines(mach,"black",True,[(165,140),(85,60),(85,140),(165,60)],width=5)
+        pygame.draw.line(mach,"black",(125,20),(125,180),width=5)
+        capt_obj = pygame.freetype.SysFont("consolas",12)
+        mach.blits(((capt_obj.render("you",fgcolor="black",rotation=45)[0],(35,60)),(capt_obj.render("cheat",fgcolor="black",rotation=45)[0],(40,65)),(capt_obj.render("you",fgcolor="black",rotation=45)[0],(75,20)),(capt_obj.render("cooperate",fgcolor="black",rotation=45)[0],(70,15)),(capt_obj.render("they",fgcolor="black",rotation=-45)[0],(150,10)),(capt_obj.render("cooperate",fgcolor="black",rotation=-45)[0],(130,10)),(capt_obj.render("they",fgcolor="black",rotation=-45)[0],(190,50)),(capt_obj.render("cheat",fgcolor="black",rotation=-45)[0],(180,60))))  
+        num_obj = pygame.freetype.SysFont("bahnschrift",24)
+        if (bottomn==None and bottom) or (bottomn):
+            mach.blits(((num_obj.render("0")[0],(105,130)),(num_obj.render("0")[0],(135,130))))
+        if (topn==None and top) or (topn):
+            mach.blits(((num_obj.render("+3")[0],(97,50)),(num_obj.render("+3")[0],(129,50))))
+        if (leftn==None and left) or (leftn):
+            mach.blits(((num_obj.render("+3")[0],(57,90)),(num_obj.render("-1")[0],(90,90))))
+        if (rightn==None and right) or (rightn):
+            mach.blits(((num_obj.render("-1")[0],(138,90)),(num_obj.render("+3")[0],(169,90))))
+        self.objects.append([mach,rect,"machine"])
+    def add_player(self,character,position,dir=0):
+        img = pygame.image.load(character+".png")
+        if dir:
+            img = pygame.transform.flip(img,1,0)
+        self.objects.append([img,position,character])
     def ready_static(self):
+        self.background.fill(self.background_color)
         for label in self.labels:
             self.background.blit(label[0],label[1])
     def ready_dynamic(self):
         for button in self.buttons:
             if button[0]==0:
                 pygame.draw.rect(self.background,button[3],button[1],border_radius=int(button[1][1]/2))
-                pygame.draw.rect(self.background,button[5],button[1],width=2,border_radius=int(button[1][1]/2))
+                text_img = button[2][2].render(button[2][0],button[2][1],bgcolor=button[3])[0]
             elif button[0]==1:
-                pygame.draw.rect(self.background,button[3],button[1],border_radius=int(button[1][1]/2))
-                pygame.draw.rect(self.background,button[6],button[1],width=2,border_radius=int(button[1][1]/2))
+                pygame.draw.rect(self.background,button[4],button[1],border_radius=int(button[1][1]/2))
+                text_img = button[2][2].render(button[2][0],button[2][1],bgcolor=button[4])[0]
             elif button[0]==2:
-                pygame.draw.rect(self.background,button[4],button[1],border_radius=int(button[1][1]/2))
-                pygame.draw.rect(self.background,button[6],button[1],width=2,border_radius=int(button[1][1]/2))
-            elif button[0]==3:
-                pygame.draw.rect(self.background,button[4],button[1],border_radius=int(button[1][1]/2))
-                pygame.draw.rect(self.background,button[5],button[1],width=2,border_radius=int(button[1][1]/2))            
-            self.background.blit(button[2],(button[1][0]+((button[1][2]-button[2].get_width())/2),button[1][1]+((button[1][3]-button[2].get_height())/2)))
+                pygame.draw.rect(self.background,button[5],button[1],border_radius=int(button[1][1]/2))
+                text_img = button[2][2].render(button[2][0],button[2][1],bgcolor=button[5])[0] 
+            pygame.draw.rect(self.background,button[6],button[1],width=button[7],border_radius=int(button[1][1]/2))         
+            self.background.blit(text_img,(button[1][0]+((button[1][2]-text_img.get_width())/2),button[1][1]+((button[1][3]-text_img.get_height())/2)))
+    def ready_objects(self):
+        for obj in self.objects:
+            self.background.blit(obj[0],obj[1])
     def flush(self):
+        pass
+    def reset(self):
         pass
     def return_subscreen(self):
         return self.background
+
+class TempValues:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,67 +1,226 @@
 def main():
-    import back    
+    import back   
+    import random as r     
+
+    def copycat(player_response_list,*useless_stuff):
+        if len(player_response_list):
+            return player_response_list[len(player_response_list)-1]
+        else:
+            return 1
     
-    s = back.Screen((1200,700),(850,650),"logo.png","Window Caption")
+    def all_cooperate(*useless_stuff):
+        return 1
+    
+    def all_cheat(*useless_stuff):
+        return 0
+    
+    def grudger(player_response_list,*useless_stuff):
+        return (not (0 in player_response_list))
+
+    def detective(player_response_list,*useless_stuff):
+        l = len(player_response_list)
+        if l in [1,3,4]:
+            return 1
+        elif l==2:
+            return 0
+        else:
+            if 0 in player_response_list[4:]:
+                return player_response_list[l-1]
+            else:
+                return 0
+
+    def copykitten(player_response_list,*useless_stuff):
+        return (player_response_list[len(player_response_list)-1] or player_response_list[len(player_response_list)-2])
+
+    def simpleton(player_response_list,bot_response_list,*useless_stuff):
+        l = len(player_response_list)
+        if l:
+            return not (player_response_list[l-1]^bot_response_list[l-1])
+        else:
+            return 1
+
+    def rando(*useless_stuff):
+        return r.choice([0,1])
+
+    char_resp_list = [copycat,all_cooperate,all_cheat,grudger,detective,copykitten,simpleton,rando]
+
+    L0 = back.Section((1200,600),'white',"Caption")
+    L1 = back.Section((1200,600),'white',"Caption")
+    L2 = back.Section((1200,600),'white',"Caption")
+    L3 = back.Section((1200,600),'white',"Caption")
+    L4 = back.Section((1200,600),'white',"Caption")
+
+    s = back.Screen((1400,700),(1300,700),"logo.png","The Evolution of Trust","grey",[L1,L2,L3])
 
     def switch_section(f,t):
-        f.enabled = False
+        if f!=0:
+            f.enabled = False
         t.enabled = True
-        t.ready_static()
         s.flush()
-    
-    L1 = back.Section((800,600),'white',False)
-    L2 = back.Section((800,600),'white',False)
-    L3 = back.Section((800,600),'white',False)
-    L4 = back.Section((800,600),'white',False)
+        t.ready_static()
+        t.ready_dynamic()
+        t.ready_objects()
+        s.before()
+        s.place_subscreen(t.return_subscreen())
+        s.after()
+        s.event_handler(t.buttons,[t.ready_dynamic,t.ready_objects],1)    
 
-    L1.add_label("First ","black","arial",36,(0,0),0)
-    L1.add_label("Page","red","arial",36,(0,0))
-    L1.add_label("Content","blue","arial",36,(0,0),1.2)
-    L1.add_button("Go to L2","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L1,L2])
-    L1.ready_static()
-    L1.ready_dynamic()
-    
-    L2.add_label("Second ","black","arial",48,(0,0),0)
-    L2.add_label("Page","orange","arial",48,(0,0))
-    L2.add_label("Content","blue","arial",48,(0,0),1.2)
-    L2.add_button("Go to L3","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L2,L3])
-    L2.ready_static()
-    L2.ready_dynamic()
-    
-    L3.add_label("Third ","red","palatinolinotype",48,(0,0),0)
-    L3.add_label("Page","green","palatinolinotype",48,(0,0))
-    L3.add_label("Content","black","palatinolinotype",48,(0,0),1.2)
-    L3.add_button("Go to L4","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L3,L4])
-    L3.ready_static()
-    L3.ready_dynamic()
-    
-    L4.add_button("Go to L1","black","calibri",20,[200,300,200,40],"white","yellow","black","green",[switch_section,L4,L1])
-    L4.ready_static()
-    L4.ready_dynamic()
+    L0.add_label("The",'black','bahnschrift',90,(600,50),-2)
+    L0.add_label("Evolution",'black','bahnschrift',90,(600,150),-2)
+    L0.add_label("of",'black','bahnschrift',90,(600,250),-2)
+    L0.add_label("Trust",'black','bahnschrift',90,(600,350),-2)
+    L0.add_button("Play!",'black','bahnschrift',24,(500,500,200,50),"white","yellow","green","black",[[switch_section,L0,L1]],border_width=1)
 
-    switch_section(L1,L1)
+    L1.add_label("During World War I, peace broke out.","black","bahnschrift",24,(100,50),0,background="white")
+    L1.add_label("It was Christmas 1914 on the Western Front. Despite strict orders not to chillax with the ","black","bahnschrift",24,(100,0),2,background="white")
+    L1.add_label("enemy, British and German soldiers left their trenches, crossed No Man's Land, and ","black","bahnschrift",24,(100,0),1.2)
+    L1.add_label("gathered to bury their dead, exchange gifts, and play games.","black","bahnschrift",24,(100,0),1.2)
+    L1.add_label("Meanwhile: it's 2022, the West has been at peace for decades, and wow, we suck at trust. ","black","bahnschrift",24,(100,0),2)
+    L1.add_label("Surveys show that, over the past forty years, fewer and fewer people say they trust each ","black","bahnschrift",24,(100,0),1.2)
+    L1.add_label("other. So here's our puzzle:","black","bahnschrift",24,(100,0),1.2)
+    L1.add_label("Why, even in peacetime, do friends become enemies?","green","bahnschrift",24,(100,0),2,italic=True)
+    L1.add_label("And why, even in wartime, do enemies become friends?","green","bahnschrift",24,(100,0),1.2,italic=True)
+    L1.add_label("I think ","black","bahnschrift",24,(100,0),2)
+    L1.add_label("game theory","red","bahnschrift",24,(100,0))
+    L1.add_label(" can help explain our epidemic of distrust - and how we can fix it! ","black","bahnschrift",24,(100,0))
+    L1.add_label("So, to understand all this...","black","bahnschrift",24,(100,0),1.2)
+    L1.add_button("...let's play a game.","black","bahnschrift",24,(450,500,300,50),"white","yellow","green","black",[[switch_section,L1,L2]])    
 
-    while 1:        
+    L2.add_label("THE GAME OF TRUST","black","bahnschrift",32,(600,50),-2)
+    L2.add_label("You have one choice. In front of you is a machine: if you put a coin in the machine, the other","black","bahnschrift",24,(100,100),0)
+    L2.add_label("player gets three coins - and vice versa. You both can either choose to COOPERATE (put in coin),","black","bahnschrift",24,(100,100),1.2)
+    L2.add_label("or CHEAT (don't put in coin).","black","bahnschrift",24,(100,100),1.2)
+    L2.add_label("you","black","bahnschrift",18,(280,220),-2)
+    L2.add_label("other player","black","bahnschrift",18,(890,220),-2)
+    L2.config_machine((475,200,250,200),0,0,0,0)
+    L2.add_player("char0",(280,330))
+    L2.add_player("char1",(900,330),1)
+    L2.add_label("Let's say the other player cheats, and doesn't put in a coin.","black","bahnschrift",20,(600,430),-2)
+    L2.add_label("What should you do?","black","bahnschrift",20,(600,460),-2,italic=1) 
+    L2v = back.TempValues()
+    L2v.stepnumber = 0
+    def L2resp(resp):
+        if L2v.stepnumber==0:
+            L2.config_machine((475,200,250,200),0,1,0,1)
+            L2v.stepnumber+=1
+            L2.labels = []
+            if resp:                
+                L2.add_label("Alas, turning the other cheek just gets you slapped!","black","bahnschrift",24,(600,70),-2)               
+            else:
+                L2.add_label("Exactly! Why let that moocher mooch off of you?","black","bahnschrift",24,(600,70),-2)
+            L2.add_label("If you cooperate & they cheat, you lose a coin while they gain three (score: -1 vs +3).","black","bahnschrift",24,(600,100),-2)
+            L2.add_label("However, if you both cheat, neither of you gain or lose anything (score: 0 vs 0).","black","bahnschrift",24,(600,130),-2)
+            L2.add_label("Therefore: you should CHEAT.","black","bahnschrift",24,(600,160),-2,bold=1) 
+            L2.add_label("But let's say the other player cooperates, and puts in a coin.","black","bahnschrift",20,(600,430),-2)
+            L2.add_label("What should you do now?","black","bahnschrift",20,(600,460),-2,italic=1) 
+        elif L2v.stepnumber==1:
+            L2.config_machine((475,200,250,200),1,1,1,1)
+            L2.labels = []
+            if resp:                
+                L2.add_label("Sure, seems like the right thing to do... OR IS IT??","black","bahnschrift",24,(600,70),-2)               
+            else:
+                L2.add_label("Wow, that's mean... and also the correct answer!","black","bahnschrift",24,(600,70),-2)
+            L2.add_label("Because if you both cooperate, you both give up a coin to gain three (score: +2 vs +2).","black","bahnschrift",24,(600,100),-2)
+            L2.add_label("But if you cheat & they cooperate, you gain three coins at their cost of one (score: +3 vs -1).","black","bahnschrift",24,(600,130),-2)
+            L2.add_label('Therefore: you "should" still CHEAT.',"black","bahnschrift",24,(600,160),-2,bold=1) 
+            L2.add_label("And that's our dilemma. Trust is nice, but it can let others take advantage of you - or shoot you as you come","black","bahnschrift",20,(100,430),0)
+            L2.add_label("unarmed out of a trench. Sometimes, distrust is rational! But now, what happens if we play this game...","black","bahnschrift",20,(100,460),1.5) 
+            L2.buttons = []
+            L2.add_button("...more than once?","black","bahnschrift",24,(450,500,300,50),"white","yellow","green","black",[[switch_section,L2,L3]])
+    L2.add_button("COOPERATE","black","bahnschrift",24,(350,500,200,50),"white","yellow","green","black",[[L2resp,1],[switch_section,L2,L2]])#[switch_section,L2,L3],
+    L2.add_button("CHEAT","black","bahnschrift",24,(650,500,200,50),"white","yellow","green","black",[[L2resp,0],[switch_section,L2,L2]])    #[switch_section,L2,L3],
+    
+    L3.add_label("Now, let's play for real. You'll be playing against 5 different opponents,","black","bahnschrift",24,(600,40),-2) 
+    L3.add_label('each with their own game "strategy". With each opponent, you\'ll play',"black","bahnschrift",24,(600,70),-2) 
+    L3.add_label("anywhere between 3 to 7 rounds (You won't know in advance when the","black","bahnschrift",24,(600,100),-2) 
+    L3.add_label("last round is). Can you trust them? Or rather... can they trust you?","black","bahnschrift",24,(600,130),-2) 
+    L3.add_label("Pick your first, real move.","black","bahnschrift",20,(600,430),-2)
+    L3.add_label("Choose wisely.","black","bahnschrift",20,(600,460),-2,bold=1)    
+    L3.config_machine((475,200,250,200),0,0,0,0,1,1,1,1)
+    L3.add_player("char1",(860,330),1)
+    L3.add_player("char0",(280,330))    
+    L3v = back.TempValues()
+    L3v.stepnumber = 0
+    L3v.character = 0
+    #L3v.number_of_rounds = [r.randint(3,7) for i in range(5)]
+    L3v.number_of_rounds = [10 for i in range(5)]
+    L3v.current_prl = []
+    L3v.current_brl = []
+    L3v.current_score = 0
+    L3v.step_score = [0,0]
+    def L3resp(resp):
+        botresp = char_resp_list[L3v.character](L3v.current_prl,L3v.current_brl)
+        L3v.current_brl.append(botresp)
+        L3v.current_prl.append(resp)      
+        lac = [0,0,0,0]
+        if botresp+resp==2:
+            lac[2]=1
+            L3v.step_score[0]+=2
+            L3v.current_score+=2
+            L3v.step_score[1]+=2            
+        elif botresp and not resp:
+            lac[1]=1
+            L3v.step_score[0]+=3
+            L3v.current_score+=3
+            L3v.step_score[1]-=1
+        elif resp and not botresp:
+            lac[0]=1
+            L3v.step_score[0]-=1
+            L3v.current_score-=1
+            L3v.step_score[1]+=3 
+        else:
+            lac[3]=1
+        L3.config_machine((475,200,250,200),*lac,1,1,1,1)  
+        L3v.stepnumber+=1
+        L3.labels = []
+        if L3v.stepnumber==L3v.number_of_rounds[L3v.character]:
+            L3v.character+=1
+            L3v.stepnumber=0
+            L3v.current_prl = []
+            L3v.current_brl = []
+            L3v.step_score = [0,0]
+            L3.add_player("char%d"%(L3v.character+1),(860,330),1)
+            L3.add_player("char0",(280,330))  
+        if L3v.character==5:
+            L3.buttons=[]
+            L3.add_button("Next","black","bahnschrift",24,(450,500,300,50),"white","yellow","green","black",[[switch_section,L3,L4]])
+            return
+        L3.add_label("opponent: %d of 5"%(L3v.character+1),"black","bahnschrift",20,(600,50),-2)
+        L3.add_label("your total score: %d"%(L3v.current_score),"black","bahnschrift",20,(600,80),-2)        
+        L3.add_label("%d ~ %d"%tuple(L3v.step_score),"black","bahnschrift",70,(600,120),-2)
+    L3.add_button("COOPERATE","black","bahnschrift",24,(350,500,200,50),"white","yellow","green","black",[[L3resp,1],[switch_section,L3,L3]])
+    L3.add_button("CHEAT","black","bahnschrift",24,(650,500,200,50),"white","yellow","green","black",[[L3resp,0],[switch_section,L3,L3]])
+
+    L4.add_button("Go to L0","black","calibri",20,(500,500,200,50),"white","yellow","green","black",[[switch_section,L4,L0]])
+
+    switch_section(0,L0)
+
+    while 1:     
+        while L0.enabled:
+            s.before()
+            s.place_subscreen(L0.return_subscreen())
+            s.after()
+            s.event_handler(L0.buttons,[L0.ready_dynamic])   
         while L1.enabled:
             s.before()
             s.place_subscreen(L1.return_subscreen())
             s.after()
-            s.event_handler(L1.buttons,L1.ready_dynamic)
+            s.event_handler(L1.buttons,[L1.ready_dynamic])
         while L2.enabled:
             s.before()
             s.place_subscreen(L2.return_subscreen())
             s.after()
-            s.event_handler(L2.buttons,L2.ready_dynamic)
+            s.event_handler(L2.buttons,[L2.ready_dynamic,L2.ready_objects])
         while L3.enabled:
             s.before()
             s.place_subscreen(L3.return_subscreen())
             s.after()
-            s.event_handler(L3.buttons,L3.ready_dynamic)
+            s.event_handler(L3.buttons,[L3.ready_dynamic])
         while L4.enabled:
             s.before()
             s.place_subscreen(L4.return_subscreen())
             s.after()
-            s.event_handler(L4.buttons,L4.ready_dynamic)
+            s.event_handler(L4.buttons,[L4.ready_dynamic])
 
 if __name__ == "__main__":
     main()
